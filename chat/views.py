8862bad2c91268f59093ae10f5deeea2ebd651c2
Lewis Bentley” <ldb2003@sky.com>
2023-02-24 15:55:52 +0000

    added views

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
+def chat(request):
+    pass
+
+
+def Login(request):
+    pass
+
+
+def MainPage(request):
+    pass
+
+
+def MyChatGLA(request):
+    pass
+
+
+def Profile(request):
+    pass
+
+
+def SignUp(request):
+    pass
+
+
 # Create your views here.
