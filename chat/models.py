9d69664a4fa0aafbc9b57a940228d15cd78bdecf
Onur Cay <2662884C@student.gla.ac.uk>
2023-02-24 21:30:37 +0000

    Added a media directory and added picture to user model

--- chat/models.py
+++ chat/models.py
@@ -11,6 +11,7 @@ class User(models.Model):
     username = models.CharField(max_length=30, unique=True, default=User.username)
     user = models.OneToOneField(DjangoUser, on_delete=models.CASCADE)
     display_name = models.CharField(max_length=50)
+    picture = models.ImageField(upload_to = 'profile_images', blank=True)
 
     def __str__(self):
         return self.username
