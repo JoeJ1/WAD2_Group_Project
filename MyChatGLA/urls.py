6b3404f730f794ea4be68b93f9cf5721d9abc747
Onur Cay <2662884C@student.gla.ac.uk>
2023-03-03 15:05:19 +0000

    Main Page work better now

--- MyChatGLA/urls.py
+++ MyChatGLA/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from chat import views
 
 urlpatterns = [
     path('chat/', include('chat.urls')),
     path('admin/', admin.site.urls),
-    path('', views.main_page, name='chat'),
+    path('', lambda request: redirect('/chat/'), name='chat'),
 ]
--- chat/models.py
+++ chat/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import User as DjangoUser
+from django.template.defaultfilters import slugify
 
 USERNAME_LEN = 30
 DISPLAY_NAME_LEN = 50
@@ -8,7 +9,6 @@ CHAT_NAME_LEN = 50
 CHAT_DESCRIPTION_LEN = 240
 
 class UserProfile(models.Model):
-    #username = models.CharField(max_length=30, unique=True, default=User.username)# django creates username automatically?
     user = models.OneToOneField(DjangoUser, on_delete=models.CASCADE)
     display_name = models.CharField(max_length=50)
     picture = models.ImageField(upload_to = 'profile_images', blank=True)
@@ -19,18 +19,23 @@ class UserProfile(models.Model):
 class Chat(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=240)
-    users = models.ManyToManyField(UserProfile)
-    owner = models.CharField(UserProfile, max_length=50)
+    users = models.ManyToManyField(UserProfile, related_name="chats")
+    owner = models.ForeignKey(UserProfile, on_delete = models.CASCADE, related_name= 'chats_owned')
+    #slug = models.SlugField(unique=True)
 
+    #def save(self, *args, **kwargs):
+    #    self.slug = slugify(self.name)
+    #    super(Chat, self).save(*args, **kwargs)
     def __str__(self):
         return self.name
 
 class Message(models.Model):
     ID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    content = models.CharField(max_length=1000)
-    display_name = models.CharField(max_length=200)#Why is this needed?
-    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
-    sender = models.CharField(max_length=30)
+    content = models.TextField()
+    display_name = models.ForeignKey(UserProfile,  on_delete= models.CASCADE, related_name = 'sender_name')#Why is this needed?
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(UserProfile, on_delete= models.CASCADE, related_name= 'sender')
+    #time_stamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"\"{self.content}\""
@@ -38,7 +43,7 @@ class Message(models.Model):
 class File(models.Model):
     ID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=256) # Max file name length on windows
-    expiry = models.DateTimeField('expiry')#Do we need an expiry? we could just delete when chat is deleted
+    #expiry = models.DateTimeField('expiry')#Do we need an expiry? we could just delete when chat is deleted
     data = models.FileField(upload_to = 'chat_files') #change upload_to
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
 
