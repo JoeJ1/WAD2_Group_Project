1728755eae07cf6ffaf02f0c19b9e7b204d3d1f4
BytePhoenixCoding <86841414+BytePhoenixCoding@users.noreply.github.com>
2023-03-10 15:43:37 +0000

    add in barebone functions

--- automated_tests/autotest.py
+++ automated_tests/autotest.py
@@ -1,3 +1,31 @@
 class BackendTest(TestCase):
-  def test_backend_1(self):
-    self.assertEqual((1 = 1), True)
+  def test_user_authentication(self):
+    self.assertEqual(True, True)
+
+  def test_display_correct_pages_if_owner_or_member(self):
+    self.assertEqual(True, True)
+
+  def test_display_only_your_classes(self):
+    self.assertEqual(True, True)
+
+  def test_able_to_create_new_accounts(self):
+    self.assertEqual(True, True)
+
+  def test_able_to_login_using_external_api(self):
+    self.assertEqual(True, True)
+    
+  def test_files_should_have_expiry_dates(self):
+    self.assertEqual(True, True)
+    
+  def test_user_can_download_view_files(self):
+    self.assertEqual(True, True)
+    
+  def test_user_can_be_owner_or_student_of_several_chats(self):
+    self.assertEqual(True, True)
+    
+  def test_user_can_create_new_classes(self):
+    self.assertEqual(True, True)
+    
+  def test_owners_can_add_members_to_chats_they_own(self):
+    self.assertEqual(True, True)
+
